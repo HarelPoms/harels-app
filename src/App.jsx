53e29a6c747d3e057d50c8efe9d39a3e1b413e17
HarelPoms <95750678+HarelPoms@users.noreply.github.com>
2023-03-09 14:30:03 +0200

    clean

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { Container } from '@mui/material';
 
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import LoginPage from './pages/LoginPage'
-import RegisterPage from './pages/RegisterPage'
 import Router from "./routes/Router"
 
 function App() {
