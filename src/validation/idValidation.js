9e34aea9b8c436df347ed62eceeb276d1fd571d6
HarelPoms <95750678+HarelPoms@users.noreply.github.com>
2023-03-16 17:25:57 +0200

    tweak id validation

--- src/validation/idValidation.js
+++ src/validation/idValidation.js
@@ -1,7 +1,8 @@
 import Joi from "joi";
 
 const validateId = (userInput, itemLimit) => {
-    let res = Joi.number().min(0).max(itemLimit).required().validate(userInput);
+    const idSchema = Joi.number().min(0).max(itemLimit).required();
+    let res = idSchema.validate(userInput);
     console.log("🚀 ~ file: idValidation.js:11 ~ validateId ~ res:", res)
     if(res.hasOwnProperty('error')) {
         return(false);
